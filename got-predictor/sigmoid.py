a05c46d0334035f7dbc207f75addc9a02f74b4cd
Luiz Cartolano <duducartolano@gmail.com>
2019-06-20 01:32:08 -0300

    ta pronto, mas ta ruim :c

--- got-predictor/sigmoid.py
+++ got-predictor/sigmoid.py
@@ -2,15 +2,11 @@ import numpy as np
 
 
 def sigmoid(z):
-	"""
-		:param: z - can be a matrix, vector or scalar
+    """
+        :param: z - can be a matrix, vector or scalar
 
-		:return: g - sigmoid de cada valor
-	"""
-	try:
-		g = 1/(1+np.exp(-z))
-	except:
-		import pdb
-		pdb.set_trace()
+        :return: g - sigmoid de cada valor
+    """
+    g = 1/(1+np.exp(-z))
 
-	return g
+    return g
